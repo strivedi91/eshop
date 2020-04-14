b139a1cdc59de3ef507dc5e4f5e2f639a43f7ec5
Sachin Trivedi <sachindtrivedi@gmail.com>
2020-04-14 22:50:31 +0530

    Added react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./Pages/homepage.component";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <Homepage></Homepage>;
+  return (
+    <Router>
+      <Homepage></Homepage>
+    </Router>
+  );
 }
 
 export default App;
